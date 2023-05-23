0edfdb8f9ea7226939bd4fe624e937ba714a1963
programmarshakil <shakljoka103@gmail.com>
2023-05-23 07:59:02 +0600

    tailwind install and config

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>page index</div>
+  <div>
+    <p class="font-medium text-white">page index</p>
+    <button class="btn">button</button>
+  </div>
 </template>
 
 <script setup></script>
